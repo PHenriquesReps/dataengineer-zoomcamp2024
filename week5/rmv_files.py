130f5a2a0b9f6de70dbb0e8206cfec9cb9897bfb
PHenriquesReps <136495830+PHenriquesReps@users.noreply.github.com>
2024-02-13 16:10:41 +0100

    week5

---
+++ week5/rmv_files.py
@@ -0,0 +1,22 @@
+import os
+
+
+def web_to_gcs(year, service):
+    for i in range(12):
+        
+        # sets the month part of the file_name string
+        month = '0'+str(i+1)
+        month = month[-2:]
+
+        # csv file_name
+        file_name_csv = f"{service}_tripdata_{year}-{month}.csv.gz"
+        file_name_parquet = f"{service}_tripdata_{year}-{month}.parquet"
+
+        os.remove(f'C:/Users/Utilizador/Documents/dataengineer-zoomcamp2024/week5/{file_name_csv}')
+        os.remove(f'C:/Users/Utilizador/Documents/dataengineer-zoomcamp2024/week5/{file_name_parquet}')
+
+
+
+web_to_gcs('2019', 'green')
+web_to_gcs('2020', 'green')
+
